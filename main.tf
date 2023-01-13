c6d2e60327b1e7e23b0d7de579d68dd482803cdf
stutzmp <3322590+stutzmp@users.noreply.github.com>
2023-01-13 15:00:20 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,4 +21,24 @@ resource "aws_instance" "web" {
   tags = {
     Name = "HelloWorld"
   }
+  
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http"
+
+  ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
 }
